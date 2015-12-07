c2ebd1563c3ac232391ada1c8cece32a2625c48e
Didier Fanchon <didier.fanchon@webtales.fr>
2015-12-07 11:02:24 +0100

    wip

--- reprise/spiders/reprise.py
+++ reprise/spiders/reprise.py
@@ -1,27 +1,35 @@
 import scrapy
 import rubedo
 import csv
+import re
 
 class RepriseSpider(scrapy.Spider):
     start_urls = []
     with open("calais.csv", 'r') as f:
         for row in csv.reader(f.read().splitlines()):
             start_urls.append('http://'+row[0])
+            print(row[1])
     name = "reprise"
+    exit()
     allowed_domains = ["calais.fr"]
 
     def parse(self, response):
-        title = response.css('h1::text').extract_first()
-        chapeau = title
-        content = response.xpath('//*[@id="content"]')
-        content = content.xpath('*[not(self::form or ancestor::form)]')
-        content = content.xpath('*[not(@id="outils" or ancestor::div/@id="outils")]')
-        texte = "".join(content.extract())
-        visuel = response.xpath('//img[contains(@src, "arton")]/@src').extract_first()
-        images = content.xpath('.//img[not (contains(@src, "arton") or contains(@src, "puce"))]/@src').extract()
-
-        rubedo.insertContent(title, title, texte, visuel, images)
-        
-
-
-            
+        originalUrl = response.request.meta['redirect_urls'][0]
+        m = re.search(r'\d+',originalUrl)
+        if m:
+            contentId = m.group(0)
+        else:
+            contentId = None
+        if contentId is not None:
+            title = response.css('h1::text').extract_first()
+            chapeau = title
+            content = response.xpath('//*[@id="content"]')
+            content = content.xpath('*[not(self::form or ancestor::form)]')
+            content = content.xpath('*[not(@id="outils" or ancestor::div/@id="outils")]')
+            texte = "".join(content.extract())
+            visuel = response.xpath('//img[contains(@src, "arton")]/@src').extract_first()
+            images = content.xpath('.//img[not (contains(@src, "arton") or contains(@src, "puce"))]/@src').extract()
+            rubedo.insertContent(contentId, title, title, texte, visuel, images)
+        else:
+            print(originalUrl)
+            pass
--- reprise/spiders/rubedo.py
+++ reprise/spiders/rubedo.py
@@ -9,22 +9,29 @@ import lxml
 import params
 import pymysql.cursors
 
-connexionString = 'mongodb://webtales:w3bt4les2015@149.202.168.50'
-dbName = 'calais'
-baseUrl = 'http://www.calais.fr/'
-
-client = MongoClient(connexionString)
-db = client[dbName]
+client = MongoClient(params.connexionString)
+db = client[params.dbName]
 fs = gridfs.GridFS(db)
 
-def insertContent(titre, chapeau, texte, visuel, images):
+mysqlConnection = pymysql.connect(host=params.host,
+                             user=params.user,
+                             password=params.password,
+                             db=params.db,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
+def insertContent(content_id, titre, resume, texte, visuel, images):
+
+    # get dates
+    dates = getDates(content_id)
+    
     # get proper encoding
     titre = titre.encode('UTF-8')
-    chapeau = titre
+    resume = titre
     
     # default values
-    typeId = "51a60bb0c1c3dac60700000e"
+    articleTypeId = "5644ac7880dd204c2a000087"
+    eventTypeId = "5644b0a380dd20200f0001ce"
     createTime = int(time.time())
     lastUpdateTime = createTime
     
@@ -44,96 +51,199 @@ def insertContent(titre, chapeau, texte, visuel, images):
     texte = lxml.html.tostring(body_lxml, encoding='UTF-8')  
     
     # PDF <a class="spip_in" href="IMG/pdf/bulletin_5_et_10_km.pdf" type="application/pdf">
-        
-    content = {
-        #"_id" : ObjectId("5659bd8c1a6c7ed3238b4621"),
-        "text" : titre,
-        "typeId" : typeId,
-        "version" : 1,
-        "online" : True,
-        "lastUpdateTime" : lastUpdateTime,
-        "createTime" : createTime,
-        "isProduct" : False,
-        "productProperties" : "",
-        "workspace" : {
-            "fields" : {
-                "image" : visuel_id
-            },
-            "status" : "published",
-            "taxonomy" : [ ],
-            "startPublicationDate" : "",
-            "endPublicationDate" : "",
-            "target" : [
-                        "",
-                        "global"
-                        ],
-                        "writeWorkspace" : "global",
-                        "pageId" : "",
-                        "maskId" : "",
-                        "blockId" : "",
-                        "i18n" : {
-                            "fr" : {
-                                "fields" : {
-                                    "text" : titre,
-                                    "urlSegment" : "url_courte",
-                                    "summary" : chapeau,
-                                    "body" : texte
-                                },
-                                "locale" : "fr"
-                        }
-                        },
-            "nativeLanguage" : "fr"
-        },
-        "live" : {
-            "fields" : {
-                "image" : visuel_id
-            },
-            "status" : "published",
-            "taxonomy" : [ ],
-            "startPublicationDate" : "",
-            "endPublicationDate" : "",
-            "target" : [
-                        "",
-                        "global"
-                        ],
-                        "writeWorkspace" : "global",
-                        "pageId" : "",
-                        "maskId" : "",
-                        "blockId" : "",
-                        "i18n" : {
-                            "fr" : {
-                                "fields" : {
-                                    "text" : titre,
-                                    "urlSegment" : "url_courte",
-                                    "summary" : chapeau,
-                                    "body" : texte
-                                },
-                                "locale" : "fr"
-                        }
-                        },
-            "nativeLanguage" : "fr"
-        },
-        "lastUpdateUser" : {
-            "id" : "5659ba5e1a6c7ed7238b456e",
-            "login" : "admin",
-            "fullName" : "admin"
-        },
-        "createUser" : {
-            "id" : "5659ba5e1a6c7ed7238b456e",
-            "login" : "admin",
-            "fullName" : "admin"
-        }
-    }
     
-    #print(content)
-    content_id = db.Contents.insert_one(content).inserted_id
+    if dates['date_debut'] is None or dates['date_fin'] is None:
+        objectType = 'article'
+    else:
+        objectType = 'event'
+        
+    if objectType == 'article':            
+        object = {
+            "text" : titre,
+            "typeId" : articleTypeId,
+            "version" : 1,
+            "online" : True,
+            "lastUpdateTime" : lastUpdateTime,
+            "createTime" : createTime,
+            "isProduct" : False,
+            "productProperties" : "",
+            "workspace" : {
+                "fields" : {
+                    "image" : visuel_id,
+                    "date" : dates['date_debut']
+                },
+                "status" : "published",
+                "taxonomy" : [ ],
+                "startPublicationDate" : "",
+                "endPublicationDate" : "",
+                "target" : [
+                            "",
+                            "global"
+                            ],
+                            "writeWorkspace" : "global",
+                            "pageId" : "",
+                            "maskId" : "",
+                            "blockId" : "",
+                            "i18n" : {
+                                "fr" : {
+                                    "fields" : {
+                                        "text" : titre,
+                                        "urlSegment" : "",
+                                        "summary" : resume,
+                                        "corps" : texte
+                                    },
+                                    "locale" : "fr"
+                            }
+                            },
+                "nativeLanguage" : "fr"
+            },
+            "live" : {
+                "fields" : {
+                    "image" : visuel_id
+                },
+                "status" : "published",
+                "taxonomy" : [ ],
+                "startPublicationDate" : "",
+                "endPublicationDate" : "",
+                "target" : [
+                            "",
+                            "global"
+                            ],
+                            "writeWorkspace" : "global",
+                            "pageId" : "",
+                            "maskId" : "",
+                            "blockId" : "",
+                            "i18n" : {
+                                "fr" : {
+                                    "fields" : {
+                                        "text" : titre,
+                                        "urlSegment" : "",
+                                        "summary" : resume,
+                                        "corps" : texte
+                                    },
+                                    "locale" : "fr"
+                            }
+                            },
+                "nativeLanguage" : "fr"
+            },
+            "lastUpdateUser" : {
+                "id" : "5659ba5e1a6c7ed7238b456e",
+                "login" : "admin",
+                "fullName" : "admin"
+            },
+            "createUser" : {
+                "id" : "5659ba5e1a6c7ed7238b456e",
+                "login" : "admin",
+                "fullName" : "admin"
+            }
+        }
+
+    if objectType == 'event':         
+        object = {
+            "text" : titre,
+            "typeId" : eventTypeId,
+            "version" : 1,
+            "online" : True,
+            "lastUpdateTime" : lastUpdateTime,
+            "createTime" : createTime,
+            "isProduct" : False,
+            "productProperties" : "",
+            "workspace" : {
+                "fields" : {
+                    "image" : visuel_id,
+                    "dateDebut" : dates['date_debut'],
+                    "dateFin" : dates['date_fin']
+                },
+                "status" : "published",
+                "taxonomy" : [ ],
+                "startPublicationDate" : "",
+                "endPublicationDate" : "",
+                "target" : [
+                            "",
+                            "global"
+                            ],
+                            "writeWorkspace" : "global",
+                            "pageId" : "",
+                            "maskId" : "",
+                            "blockId" : "",
+                            "i18n" : {
+                                "fr" : {
+                                    "fields" : {
+                                        "text" : titre,
+                                        "urlSegment" : "",
+                                        "summary" : resume,
+                                        "corps" : texte
+                                    },
+                                    "locale" : "fr"
+                            }
+                            },
+                "nativeLanguage" : "fr"
+            },
+            "live" : {
+                "fields" : {
+                    "image" : visuel_id
+                },
+                "status" : "published",
+                "taxonomy" : [ ],
+                "startPublicationDate" : "",
+                "endPublicationDate" : "",
+                "target" : [
+                            "",
+                            "global"
+                            ],
+                            "writeWorkspace" : "global",
+                            "pageId" : "",
+                            "maskId" : "",
+                            "blockId" : "",
+                            "i18n" : {
+                                "fr" : {
+                                    "fields" : {
+                                        "text" : titre,
+                                        "urlSegment" : "",
+                                        "summary" : resume,
+                                        "body" : texte
+                                    },
+                                    "locale" : "fr"
+                            }
+                            },
+                "nativeLanguage" : "fr"
+            },
+            "lastUpdateUser" : {
+                "id" : "5659ba5e1a6c7ed7238b456e",
+                "login" : "admin",
+                "fullName" : "admin"
+            },
+            "createUser" : {
+                "id" : "5659ba5e1a6c7ed7238b456e",
+                "login" : "admin",
+                "fullName" : "admin"
+            }
+        }
+    
+    content_id = db.Contents.insert_one(object).inserted_id
+
+def getDates(content_id):
+    with mysqlConnection.cursor() as cursor:
+        dates = {
+            'date_debut' : None,
+            'date_fin' : None
+        }
+        # Read a single record
+        sql = "SELECT date, date_redac FROM spip_articles WHERE id_article=" + content_id
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        if result['date']:
+            dates['date_debut'] = str(int(time.mktime(result['date'].timetuple())))
+        if result['date_redac']:
+            dates['date_fin'] = str(int(time.mktime(result['date_redac'].timetuple())))
+        return(dates)
 
 def insertDAM(visuel):
     
     fileName = os.path.basename(visuel)
     damObject = db.Dam.find_one({'title':fileName},{'_id':1})
     if (damObject is None):
-        filePath = baseUrl + visuel
+        filePath = params.baseUrl + visuel
         contentType, fileEncoding =  mimetypes.guess_type(filePath)
         image = urllib2.urlopen(filePath)
         meta = image.info()
@@ -182,10 +292,3 @@ def insertDAM(visuel):
     else:
         dam_id = damObject['_id']
     return dam_id
-
-def checksum_md5(self, filename):
-    try: 
-        image = urllib2.urlopen(filename)
-        return base64.encodestring(image.read())
-    except:
-        return None
