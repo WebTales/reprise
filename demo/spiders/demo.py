8b8a4749963292b424354b714a6f072b93d170d6
Didier Fanchon <didier.fanchon@webtales.fr>
2016-01-05 08:49:43 +0100

    Add demo

---
+++ demo/spiders/demo.py
@@ -0,0 +1,39 @@
+import scrapy
+import rubedo
+import csv
+import re
+import params
+from scrapy.http import Request
+
+class DemoSpider(scrapy.Spider):
+
+    name = "demo"
+    start_urls = []
+    with open(params.file, 'r') as f:
+        for row in csv.reader(f.read().splitlines(),delimiter=';'):
+            start_urls.append('http://'+row[0])
+
+    def start_requests(self):
+        for indx, url in enumerate(self.start_urls):
+            yield self.make_requests_from_url(url, {'index': indx})
+    
+    def make_requests_from_url(self, url, meta):
+       return Request(url, callback=self.parse, dont_filter=True, meta=meta)
+                   
+    def parse(self, response):
+    
+        title = response.css('h1::text').extract_first()
+        subtitle = response.xpath('//*[@class="detail-title_subtitle"]/text()')
+        
+        print(title)
+        print(subtitle)
+        
+        #chapeau = title
+        #content = response.xpath('//*[@id="content"]')
+        #content = content.xpath('*[not(self::form or ancestor::form)]')
+        #content = content.xpath('*[not(@id="outils" or ancestor::div/@id="outils")]')
+        #texte = "".join(content.extract())
+        #visuel = response.xpath('//img[contains(@src, "arton")]/@src').extract_first()
+        
+        #rubedo.insertContent(contentId, title, title, texte, visuel, params.type, "", "")
+
