04120b396612b5083003598c2312a535ec47f90e
Didier Fanchon <didier.fanchon@webtales.fr>
2016-11-03 12:01:12 +0100

    update params

--- discogs/params.py
+++ discogs/params.py
@@ -1,13 +1,13 @@
 dbName='rubedo'
 baseUrl=''
-connexionString='10.7.0.144,10.7.0.142/?replicaSet=MONGO-webtales-test'
+connexionString='10.7.0.113,10.7.0.115/?replicaSet=MONGO-wbtls-prod'
 createUserId='52298672b58ce90823000000'
 releaseTypeId='5800849472a0feac018bac0c'
 artistTypeId='5804abf072a0fe79018b83fb'
-damTypeId='52298661b58ce90810000006'
+damTypeId='51a60c1cc1c3da0407000007'
 vocabularies={
     'styles' : '58008eb072a0fe94148bb6af',
-    'genres' : '58008f0b72a0fe19008b658e',
+    'genres' : '58008f0b72a0fe19008b658e'
     'formats' : '58008eea72a0fe75018b63b3'
 }
 fields={
